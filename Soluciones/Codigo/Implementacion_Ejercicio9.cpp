60a784fe67339f9a36ed9059ddddfe2e5c00c680
Alexander Moon <A00574602@tec.mx>
2025-05-22 07:20:51 -0600

    Create Implementacion_Ejercicio9.cpp

---
+++ Soluciones/Codigo/Implementacion_Ejercicio9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Sensor {
+protected:
+    void detectarMovimiento() {
+        cout << "Movimiento detectado" << endl;
+    }
+};
+
+class SistemaAlarma : private Sensor {
+public:
+    void verificar() {
+        detectarMovimiento();
+    }
+};
+
+int main() {
+    SistemaAlarma sa;
+    sa.verificar();
+    return 0;
+}
