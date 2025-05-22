64ec5f45195ab967e18ccedacd341ef8ed7c0f51
Alexander Moon <A00574602@tec.mx>
2025-05-22 11:54:57 -0600

    Update Completar_Ejercicio11.cpp

--- Soluciones/Codigo/Completar_Ejercicio11.cpp
+++ Soluciones/Codigo/Completar_Ejercicio11.cpp
@@ -2,10 +2,24 @@
 #include <string>
 using namespace std;
 
-class Dispositivo { public: string marca; };
-class Tablet : virtual public Dispositivo {};
-class Telefono : virtual public Dispositivo {};
-class Smartphone : public Tablet, public Telefono {};
+class Dispositivo { 
+public: 
+string marca; 
+Dispositivo(const string& marca = ""):marca(marca){}
+};
+
+class Tablet : virtual public Dispositivo {
+public:
+Tablet():Dispositivo(marca){}
+};
+class Telefono : virtual public Dispositivo {
+public:
+Telefono():Dispositivo(marca){}
+};
+class Smartphone : public Tablet, public Telefono {
+public:
+Smartphone():Tablet(),Telefono(){}
+};
 
 int main() {
     Smartphone s;
