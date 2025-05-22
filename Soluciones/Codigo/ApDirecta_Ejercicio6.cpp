62f5b8665ca551df04bc1b29edb18e33b59182ec
Alexander Moon <A00574602@tec.mx>
2025-05-22 11:46:28 -0600

    Update ApDirecta_Ejercicio6.cpp

--- Soluciones/Codigo/ApDirecta_Ejercicio6.cpp
+++ Soluciones/Codigo/ApDirecta_Ejercicio6.cpp
@@ -5,18 +5,21 @@ using namespace std;
 
 class Usuario{
   public:
+  Usuario(){}
   void login(){
       cout<<"El usuario ha entrado al sistema"<<endl;
   }
 };
 class Moderador:public Usuario{
     public:
+    Moderador():Usuario(){}
     void moderar(){
         cout<<"El moderador esta moderando u)u"<<endl;
     }
 };
 class Admin:public Moderador{
     public:
+    Admin():Moderador(){}
     void banear(){
         cout<<"El administrador ha decidido banearte"<<endl;
     }
